ed7b45e29e4d77240c4ba637d00465354ddd23ca
Nathan Zhu <zhuzx2006@outlook.com>
2024-03-24 23:10:25 +0800

    version 0.08 save records first step

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ azure_settings = config['azure']
 wx_settings = config['wx_public_service']
 
 client_id = azure_settings['client_id']
-scope_list = azure_settings['scope_list']
+scope_list = azure_settings['scope_list'].replace(' ','').split(',')
 wx_APPID = wx_settings['wx_APPID']
 wx_SECRET = wx_settings['wx_SECRET']
 template_id = wx_settings['template_id']  # 在微信公众平台获取模板ID
@@ -64,8 +64,8 @@ if not result:
         "code": {"value": flow['user_code']},
     }
     # 推送消息
-    result = send_template_message(openid, template_id, data)
-    print(result)  # 打印推送结果
+    result1 = send_template_message(openid, template_id, data)
+    print(result1)  # 打印推送结果
 
     # Ideally you should wait here, in order to save some unnecessary polling
     # input("Press Enter after signing in from another device to proceed, CTRL+C to abort.")
@@ -76,8 +76,9 @@ if not result:
         # or you may even turn off the blocking behavior,
         # and then keep calling acquire_token_by_device_flow(flow) in your own customized loop
 
+days_number = int(input("Please enter the number of days to extract the information from Teams Shifts API: \n"))
 day_one = datetime.date.today()
-day_seven_ago = day_one - datetime.timedelta(days=7)
+day_seven_ago = day_one - datetime.timedelta(days=days_number)
 
 endpoint = "https://graph.microsoft.com/beta/teams/28887499-6bc5-4b2f-a06c-25cc971e30ca/schedule/timeCards?$filter=(ClockInEvent/DateTime ge {}T00:00:00Z and ClockInEvent/DateTime le {}T23:59:59Z)".format(day_seven_ago,day_one)
 http_headers = {'Authorization': 'Bearer ' + result['access_token'],
@@ -85,8 +86,33 @@ http_headers = {'Authorization': 'Bearer ' + result['access_token'],
                 'Content-Type': 'application/json'}
 data = requests.get(endpoint, headers=http_headers, stream=False).json()
 output = DataFrame(data['value'])
-print(output)
-# output.to_csv('output.csv',mode='a',header=0, index=0, encoding='utf_8_sig')
+
+time_format = "%Y-%m-%dT%H:%M:%S"
+
+learning_records = []
+learning_records.append(['Person', 'Start Time', 'End Time', 'Duration', 'Notes'])
+
+for i in range(0, len(output)):
+    output_temp = []
+    learning_person = output.values[i][8]['user']['displayName'] # user name
+    learning_start_time = datetime.datetime.strptime(output.values[i][9]['dateTime'][:-1].split('.')[0],time_format) # start time
+    learning_start_time = learning_start_time + datetime.timedelta(hours=8) # convert to China Local Time
+    learning_end_time = datetime.datetime.strptime(output.values[i][10]['dateTime'][:-1].split('.')[0],time_format) # end time
+    learning_end_time = learning_end_time + datetime.timedelta(hours=8) # convert to China Local Time
+    learning_duration = round((learning_end_time - learning_start_time).seconds/60/60,2) # hours
+    if output.values[i][7] == None:
+        learning_notes = ''
+    else:
+        learning_notes = output.values[i][7]['content']
+    output_temp.append(learning_person)
+    output_temp.append(learning_start_time)
+    output_temp.append(learning_end_time)
+    output_temp.append(learning_duration)
+    output_temp.append(learning_notes)
+    learning_records.append(output_temp)
+learning_records = DataFrame(learning_records)
+print(learning_records)
+learning_records.to_csv('Learning_records.csv',mode='a',header=0, index=0, encoding='utf_8_sig')
 
 ### Below are OneDrive Operations ###
 onedrive_url = 'https://graph.microsoft.com/v1.0/'
@@ -96,4 +122,5 @@ if (onedrive_response.status_code == 200):
     onedrive_response = json.loads(onedrive_response.text)
     items = onedrive_response['value']
     for entries in range(len(items)):
-        print(items[entries]['name'], '| item-id >', items[entries]['id'])
+        print(items[entries]['name'], '| item-id >', items[entries]['id']) # to show the files ID, which could be used in the onedrive API call
+
