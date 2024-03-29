4c1b47d989d6bf735a387281ee3ccb1506851172
zhuzx <zhuzx2006@outlook.com>
2024-03-29 10:23:34 +0800

    version 0.15 bug fix under the condition for no learning records

--- main.py
+++ main.py
@@ -100,72 +100,75 @@ http_headers = {'Authorization': 'Bearer ' + result['access_token'],
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'}
 data = requests.get(endpoint, headers=http_headers, stream=False).json()
-output = DataFrame(data['value'])
+output = data['value']
 
-time_format = "%Y-%m-%dT%H:%M:%S"
+if output != []: # if there is  learning records, continue below codes.
+    output = DataFrame(output)
+    time_format = "%Y-%m-%dT%H:%M:%S"
 
-learning_record = {'values':[]}
-learning_records = []
-# learning_records.append(['Person', 'Start Time', 'End Time', 'Duration', 'Notes'])
+    learning_record = {'values':[]}
+    learning_records = []
+    # learning_records.append(['Person', 'Start Time', 'End Time', 'Duration', 'Notes'])
 
-for i in range(0, len(output)):
-    output_temp = []
-    learning_person = output.values[i][8]['user']['displayName'] # user name
-    learning_start_time = datetime.datetime.strptime(output.values[i][9]['dateTime'][:-1].split('.')[0],time_format) # start time
-    learning_start_time = learning_start_time + datetime.timedelta(hours=8) # convert to China Local Time
-    learning_end_time = datetime.datetime.strptime(output.values[i][10]['dateTime'][:-1].split('.')[0],time_format) # end time
-    learning_end_time = learning_end_time + datetime.timedelta(hours=8) # convert to China Local Time
-    learning_duration = round((learning_end_time - learning_start_time).seconds/60/60,2) # hours
-    if output.values[i][7] == None:
-        learning_notes = ''
+    for i in range(0, len(output)):
+        output_temp = []
+        learning_person = output.values[i][8]['user']['displayName'] # user name
+        learning_start_time = datetime.datetime.strptime(output.values[i][9]['dateTime'][:-1].split('.')[0],time_format) # start time
+        learning_start_time = learning_start_time + datetime.timedelta(hours=8) # convert to China Local Time
+        learning_end_time = datetime.datetime.strptime(output.values[i][10]['dateTime'][:-1].split('.')[0],time_format) # end time
+        learning_end_time = learning_end_time + datetime.timedelta(hours=8) # convert to China Local Time
+        learning_duration = round((learning_end_time - learning_start_time).seconds/60/60,2) # hours
+        if output.values[i][7] == None:
+            learning_notes = ''
+        else:
+            learning_notes = output.values[i][7]['content']
+        output_temp.append(learning_person)
+        output_temp.append(learning_start_time.strftime("%Y-%m-%d %H:%M:%S"))
+        output_temp.append(learning_end_time.strftime("%Y-%m-%d %H:%M:%S"))
+        output_temp.append(learning_duration)
+        output_temp.append(learning_notes)
+        learning_records.append(output_temp)
+    learning_record['values'] = learning_records
+    learning_record = json.dumps(learning_record, indent=4)
+    print("Below is the learning record: \n")
+    print(learning_record)
+    # learning_records.to_csv('Learning_records.csv',mode='a',header=0, index=0, encoding='utf_8_sig') #Files\Learning\Learning_records.csv in OneDrive for Business CN
+
+    onedrive_url = 'https://graph.microsoft.com/v1.0/'
+    body_create_seesion = {'persistChanges': 'true'}
+    body_create_seesion = json.dumps(body_create_seesion, indent=4)
+
+    ### create a seesion id ###
+    onedrive_create_session =  requests.post(onedrive_url + 'me/drive/items/01L7SVHITF3Z5SOUHNWNAJVRY7EBZG2EXY/workbook/createSession', headers = http_headers, data = body_create_seesion)
+    print('Create session:: status code is: ',onedrive_create_session.status_code)
+    session_id = json.loads(onedrive_create_session.text)['id']
+
+    ### Below are OneDrive Operations ###
+    # onedrive_response = requests.get(onedrive_url + 'me/drive/root/children', headers = http_headers)
+    http_headers['Workbook-Session-Id'] = session_id
+    onedrive_response = requests.post(onedrive_url + 'me/drive/items/01L7SVHITF3Z5SOUHNWNAJVRY7EBZG2EXY/workbook/tables/Table1/rows/add', headers = http_headers, data = learning_record)
+    if (onedrive_response.status_code == 201):
+        print('item added to Onedrive for Business Learning_records.xlsx')
+        data = {
+            "code": {"value": "Run Succeed! Check Onedrive for Buiness Learning_record.xlsx"},
+        }
     else:
-        learning_notes = output.values[i][7]['content']
-    output_temp.append(learning_person)
-    output_temp.append(learning_start_time.strftime("%Y-%m-%d %H:%M:%S"))
-    output_temp.append(learning_end_time.strftime("%Y-%m-%d %H:%M:%S"))
-    output_temp.append(learning_duration)
-    output_temp.append(learning_notes)
-    learning_records.append(output_temp)
-learning_record['values'] = learning_records
-learning_record = json.dumps(learning_record, indent=4)
-print("Below is the learning record: \n")
-print(learning_record)
-# learning_records.to_csv('Learning_records.csv',mode='a',header=0, index=0, encoding='utf_8_sig') #Files\Learning\Learning_records.csv in OneDrive for Business CN
+        print('Failed to add item to Onedrive for Business Learning_records.xlsx!')
+        data = {
+            "code": {"value": "Failed, Check Github"},
+        }
+    send_template_message(openid, template_id, data)    # 推送消息
 
-onedrive_url = 'https://graph.microsoft.com/v1.0/'
-body_create_seesion = {'persistChanges': 'true'}
-body_create_seesion = json.dumps(body_create_seesion, indent=4)
+    ### close session ###
+    onedrive_close_session =  requests.post(onedrive_url + 'me/drive/items/01L7SVHITF3Z5SOUHNWNAJVRY7EBZG2EXY/workbook/closeSession', headers = http_headers)
+    if onedrive_close_session.status_code == 204:
+        print("Close session successfully!")
+    else:
+        print('Close session failed, status code is: ',onedrive_close_session.status_code)
 
-### create a seesion id ###
-onedrive_create_session =  requests.post(onedrive_url + 'me/drive/items/01L7SVHITF3Z5SOUHNWNAJVRY7EBZG2EXY/workbook/createSession', headers = http_headers, data = body_create_seesion)
-print('Create session:: status code is: ',onedrive_create_session.status_code)
-session_id = json.loads(onedrive_create_session.text)['id']
-
-### Below are OneDrive Operations ###
-# onedrive_response = requests.get(onedrive_url + 'me/drive/root/children', headers = http_headers)
-http_headers['Workbook-Session-Id'] = session_id
-onedrive_response = requests.post(onedrive_url + 'me/drive/items/01L7SVHITF3Z5SOUHNWNAJVRY7EBZG2EXY/workbook/tables/Table1/rows/add', headers = http_headers, data = learning_record)
-if (onedrive_response.status_code == 201):
-    print('item added to Onedrive for Business Learning_records.xlsx')
-    data = {
-        "code": {"value": "Run Succeed! Check Onedrive for Buiness Learning_record.xlsx"},
-    }
+        # onedrive_response = json.loads(onedrive_response.text)
+        # items = onedrive_response['value']
+        # for entries in range(len(items)):
+        #     print(items[entries]['name'], '| item-id >', items[entries]['id']) # to show the files ID, which could be used in the onedrive API call
 else:
-    print('Failed to add item to Onedrive for Business Learning_records.xlsx!')
-    data = {
-        "code": {"value": "Failed, Check Github"},
-    }
-send_template_message(openid, template_id, data)    # 推送消息
-
-### close session ###
-onedrive_close_session =  requests.post(onedrive_url + 'me/drive/items/01L7SVHITF3Z5SOUHNWNAJVRY7EBZG2EXY/workbook/closeSession', headers = http_headers)
-if onedrive_close_session.status_code == 204:
-    print("Close session successfully!")
-else:
-    print('Close session failed, status code is: ',onedrive_close_session.status_code)
-
-    # onedrive_response = json.loads(onedrive_response.text)
-    # items = onedrive_response['value']
-    # for entries in range(len(items)):
-    #     print(items[entries]['name'], '| item-id >', items[entries]['id']) # to show the files ID, which could be used in the onedrive API call
-
+    print('No learning records!')
