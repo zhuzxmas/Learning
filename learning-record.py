9bf1395de8a07f3f8b412f15e7e58e4180962914
zhuzx <zhuzx2006@outlook.com>
2024-06-06 12:10:32 +0800

    version 0.18 introduce OneNote

--- learning-record.py
+++ learning-record.py
@@ -1,129 +1,12 @@
-import json, requests, datetime, configparser, os
+import json, requests, datetime, os
 from pandas import DataFrame
-from msal import PublicClientApplication
+import funcLG
 
-config = configparser.ConfigParser()
-
-if os.path.exists('./config.cfg'): # to check if local file config.cfg is available, for local running application
-    config.read(['config.cfg'])
-    azure_settings = config['azure']
-    wx_settings = config['wx_public_service']
-    proxy_settings = config['proxy_add']
-
-    client_id = azure_settings['client_id']
-    wx_APPID = wx_settings['wx_APPID']
-    wx_SECRET = wx_settings['wx_SECRET']
-    template_id = wx_settings['template_id']  # 在微信公众平台获取模板ID
-    openid = wx_settings['openid']  # 用户的openid，可以在用户管理页面获取
-    # https://mp.weixin.qq.com/debug/cgi-bin/sandboxinfo?action=showinfo&t=sandbox/index
-    proxy_add = proxy_settings['proxy_add']
-    # days_number = int(input("Please enter the number of days to extract the information from Teams Shifts API: \n"))
-    days_number = 7
-else: # to get this info from Github Secrets, for Github Action running application
-    client_id = os.environ['client_id']
-    wx_APPID = os.environ['wx_APPID']
-    wx_SECRET = os.environ['wx_SECRET']
-    template_id = os.environ['template_id']
-    openid = os.environ['openid']
-    proxy_add = os.environ['proxy_add']
-    days_number = 7
-
-config.read(['config1.cfg']) # to get the scopes
-azure_settings_scope = config['azure1']
-scope_list = azure_settings_scope['scope_list'].replace(' ','').split(',')
-# print( 'Scope List is: ', scope_list, '\n')
-
-proxies = {
-  "http": proxy_add,
-  "https": proxy_add
-}
-
-### to create msal connection ###
-try:
-    app = PublicClientApplication(
-        client_id=client_id,
-        authority = 'https://login.microsoftonline.com/common',
-    )
-except:
-    app = PublicClientApplication(
-        client_id=client_id,
-        authority = 'https://login.microsoftonline.com/common',
-        proxies = proxies
-    )
-
-result = None
-
-# Firstly, check the cache to see if this end user has signed in before...
-accounts = app.get_accounts(username='zhuzx@cnmas.onmicrosoft.com')
-if accounts:
-    result = app.acquire_token_silent(scope_list, account=accounts[0])
-
-if not result:
-    print("No suitable token exists in cache. Let's get a new one from Azure AD.")
-
-    flow = app.initiate_device_flow(scopes=scope_list)
-    if "user_code" not in flow:
-        raise ValueError(
-            "Fail to create device flow. Err: %s" % json.dumps(flow, indent=4))
-
-    # print(flow["message"])
-    # print(f"user_code is: {flow['user_code']}, login address: {flow['verification_uri']}")
-
-    # 获取access_token
-    def get_access_token():
-        url = f"https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={wx_APPID}&secret={wx_SECRET}"
-        try:
-            response = requests.get(url)
-        except:
-            response = requests.get(url, proxies=proxies)
-
-        data = response.json()
-        access_token = data.get("access_token")
-        return access_token
-
-    # 推送模板消息
-    def send_template_message(openid, template_id, data):
-        access_token = get_access_token()
-        url = f"https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={access_token}"
-        headers = {'Content-Type': 'application/json'}
-        login_info = {
-            "touser": openid,
-            "template_id": template_id,
-            "data": data
-        }
-        try:
-            response = requests.post(url, headers=headers, data=json.dumps(login_info))
-        except:
-            response = requests.post(url, headers=headers, data=json.dumps(login_info),proxies=proxies)
-        return response.json()
-
-    # 示例数据
-    data = {
-        "code": {"value": flow['user_code']},
-    }
-    # 推送消息
-    result1 = send_template_message(openid, template_id, data)
-    # print(result1)  # 打印推送结果
-
-    # Ideally you should wait here, in order to save some unnecessary polling
-    # input("Press Enter after signing in from another device to proceed, CTRL+C to abort.")
-
-    result = app.acquire_token_by_device_flow(flow)  # By default it will block
-        # You can follow this instruction to shorten the block time
-        #    https://msal-python.readthedocs.io/en/latest/#msal.PublicClientApplication.acquire_token_by_device_flow
-        # or you may even turn off the blocking behavior,
-        # and then keep calling acquire_token_by_device_flow(flow) in your own customized loop
-
-#### update OneNote Title ###
-#endpoint = 'https://graph.microsoft.com/v1.0/me/onenote/pages/1-5550639376b04dafbe186ccd402d4983!19-642556db-9640-4ed4-9c07-5995cf8a4824/content'
-#http_headers = {'Authorization': 'Bearer ' + result['access_token'],
-#                'Content-Type': 'application/json'}
-#testvalue = [{
-#    'target':'title',
-#    'action':'replace',
-#    'content':'New title'
-#  }]
-#data = requests.patch(endpoint, headers=http_headers, data=json.dumps(testvalue,indent=4))
+login_return = funcLG.func_login() # to login into MS365 and get the return value
+result = login_return['result']
+proxies = login_return['proxies']
+days_number = 7
+# days_number = int(input("Please enter the number of days to extract the information from Teams Shifts API: \n"))
 
 day_one = datetime.date.today()
 day_seven_ago = day_one - datetime.timedelta(days=days_number)
@@ -206,7 +89,9 @@ if output != []: # if there is  learning records, continue below codes.
         data = {
             "code": {"value": "Failed, Check Github"},
         }
-    send_template_message(openid, template_id, data)    # 推送消息
+    openid = login_return['openid']
+    template_id = login_return['template_id']
+    funcLG.send_template_message(openid, template_id, data)    # 推送消息
 
     ### close session ###
     try:
