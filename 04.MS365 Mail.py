32069b0d1e859860f0216f4091f326df9b39c7a5
zhuzx <zhuzx2006@outlook.com>
2024-09-18 22:01:31 +0800

    version 0.37 update MS365 Mail API service

--- 04.MS365 Mail.py
+++ 04.MS365 Mail.py
@@ -3,10 +3,12 @@
 Created on Sep 18, 2024  @author: Nathan. File Purpose: to use MS365 Graph API for mail service.
 """
 import urllib.request
+import pprint
 import requests
 import os.path
 import json
 import funcLG
+
 
 def main():
 
@@ -32,7 +34,8 @@ def main():
             user_id = data['value'][i]['id']
 
     # to list mail folders:
-    endpoint = 'https://graph.microsoft.com/v1.0/users/{}/mailFolders'
+    endpoint = 'https://graph.microsoft.com/v1.0/users/{}/mailFolders'.format(
+        user_id)
     http_headers = {'Authorization': 'Bearer ' + result['access_token'],
                     'Accept': 'application/json',
                     'Content-Type': 'application/json'}
@@ -45,13 +48,13 @@ def main():
                             stream=False, proxies=proxies).json()
     for i in range(0, len(data['value'])):
         if data['value'][i]['displayName'] == 'Inbox':
-            mailFolder_id = data['value'][i]['id']
+            mailFolder_inbox_id = data['value'][i]['id']
+        if data['value'][i]['displayName'] == 'Inbox':
+            mailFolder_inbox_id = data['value'][i]['id']
 
-
-
-    # to get the user latest 10 emails list.
+    # to get the user latest 10 emails list with descent order with desc in the endpoint.
     endpoint = 'https://graph.microsoft.com/v1.0/users/{}/mailFolders/{}/messages?$orderby=receivedDateTime desc'.format(
-        user_id, mailFolder_id)
+        user_id, mailFolder_inbox_id)
     try:
         data = requests.get(endpoint, headers=http_headers,
                             stream=False).json()
@@ -64,9 +67,37 @@ def main():
         email_temp.append(data['value'][i]['subject'])
         email_temp.append(data['value'][i]['from']['emailAddress']['name'])
         email_list.append(email_temp)
-    print(email_list)
+    pprint.pprint(email_list)
 
 
+    # to send an email to someone
+    email_message= {
+        'message': {
+            'subject': 'Test email from Microsoft Graph API',
+            'body': {
+                'contentType': 'Text',
+                'content': 'Hello, this is a test email sent using the Microsoft Graph API.'
+            },
+            'toRecipients': [
+                {
+                    'emailAddress': {
+                        'address': 'zhuzx2006@outlook.com'
+                    }
+                }
+            ]
+        },
+        'saveToSentItems': True
+    },
+    # email_message = json.dumps(email_message, indent=4)
+    email_message = json.dumps(email_message)
+    endpoint = 'https://graph.microsoft.com/v1.0/users/{}/sendMail'.format(
+        user_id)
+    try:
+        data = requests.post(endpoint, headers=http_headers,
+                             stream=False, data=email_message).json()
+    except:
+        data = requests.get(endpoint, headers=http_headers,
+                            stream=False, proxies=proxies, data=email_message).json()
 
 
 main()
