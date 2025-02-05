4c297cc3af9a1d9c59e364e48c4edcd9c9991ed0
ZZHU25 <zzhu25@ford.com>
2025-02-05 17:38:38 +0800

    version 1.17 finance func: stable version

--- 01.finance-batch-quarterly.py
+++ 01.finance-batch-quarterly.py
@@ -23,7 +23,7 @@ stock_code = ['F', 600885, 603259, 600276, 600196, 603899, 603986, 603833, 60380
 # stock_code = [600885, 603259, 600276]
 
 # stock_name = ['宏发股份', '中芯国际', '天合光能', '奇安信', '华润微', '华熙生物', '大全能源', '联影医疗', '寒武纪', '晶科能源', '时代电气', '沪硅产业', '金山办公', '凯赛生物', '海光信息', '传音控股', '中微公司', '澜起科技', '东鹏饮料', '德业股份', '洛阳钼业', '兆易创新', '晨光股份', '欧派家居', '福斯特', '华友钴业', '璞泰来', '韦尔股份', '科沃斯', '万泰生物', '今世缘', '斯达半导', '海天味业', '合盛硅业', '药明康德', '公牛集团', '中科曙光', '中信银行', '中金公司', '中国重工', '中国银行', '中国核电', '建设银行', '中远海控', '浙商银行', '方正证券', '紫金矿业', '中煤能源', '中国中免', '中国银河', '浙商证券', '正泰电器', '招商轮船', '中国能建', '福莱特', '中国石油', '成都银行', '光大银行', '京沪高铁', '中海油服', '中国交建', '星宇股份', '光大证券', '中国中车', '中国电信', '潞安环能', '中国卫通', '拓普集团', '华泰证券', '中国电建', '中国建筑', '邮储银行', '长城汽车', '中国人寿', '中国中冶', '明阳智能', '上海医药', '中国太保', '中国铝业', '工商银行', '中国中铁', '兴业证券', '三六零', '新华保险', '交通银行', '中国人保', '中国平安', '农业银行', '广汽集团', '红塔证券', '上海银行', '陕西煤业', '国泰君安', '中国铁建', '北京银行', '兴业银行', '新城控股', '工业富联', '中国化学', '中国国航', '恒立液压', '中国神华', '中信建投', '信达证券', '春秋航空', '隆基绿能', '南京银行', '大秦铁路', '招商证券', '宝丰能源', '东方证券', '中国移动', '中国海油', '杭州银行', '江苏银行', '中泰证券', '三峡能源', '长江电力', '航发动力', '伊利股份', '国投电力', '东方电气', '宝信软件', '海通证券', '山西汾酒', '新奥股份', '国电电力', '中航沈飞', '锦江酒店', '闻泰科技', '华域汽车', '爱旭股份', '海尔智家', '川投能源', '福耀玻璃', '绿地控股', '青岛啤酒', '用友网络', '海螺水泥', '长电科技', '恒生电子', '山东黄金', '贵州茅台', '海南机场', '中金黄金', '士兰微', '通威股份', '片仔癀', '华鲁恒升', '国电南瑞', '中航机载', '江西铜业', '恒力石化', '白云山', '万华化学', '恒瑞医药', '圆通速递', '南山铝业', '复星医药', '兖矿能源', '生益科技', '中国巨石', '中国船舶', '重庆啤酒', '中国东航', '北方稀土', '上汽集团', '特变电工', '同仁堂', '国投资本', '中国联通', '保利发展', '四川路桥', '招商银行', '三一重工', '中信证券', '南方航空', '中国石化', '华能水电', '浙能电力', '宝钢股份', '上港集团', '民生银行', '华夏银行', '华能国际', '包钢股份', '上海机场', '浦发银行', '华大九天', '金龙鱼', '华利集团', '贝泰妮', '中伟股份', '爱美客', '卓胜微', '锦浪科技', '迈瑞医疗', '康龙化成', '迈为股份', '宁德时代', '圣邦股份', '亿联网络', '温氏股份', '中科创达', '深信服', '先导智能', '蓝思科技', '芒果超媒', '三环集团', '泰格医药', '晶盛机电', '中际旭创', '阳光电源', '北京君正', '沃森生物', '汇川技术', '智飞生物', '东方财富', '同花顺', '爱尔眼科', '亿纬锂能', '中国广核', '鹏鼎控股', '德赛西威', '深南电路', '视源股份', '凯莱英', '恩捷股份', '国信证券', '牧原股份', '天赐材料', '卫星化学', '以岭药业', '龙佰集团', '比亚迪', '三七互娱', '荣盛石化', '立讯精密', '天齐锂业', '赣锋锂业', '晶澳科技', '海康威视', '广联达', '北方华创', '顺丰控股', '海大集团', '洋河股份', '东方雨虹', '上海莱士', '歌尔股份', '大华股份', '科大讯飞', '金风科技', '纳思达', '中航光电', '宁波银行', 'TCL中环', '国轩高科', '三花智控', '紫光国微', '分众传媒', '华兰生物', '新和成', '招商蛇口', '龙源电力', '华润三九', '山西焦煤', '浪潮信息', '华东医药', '紫光股份', '双汇发展', '天山股份', '新希望', '五粮液', '一汽解放', '盐湖股份', '北新建材', '广发证券', '中航西飞', '振华科技', '京东方A', '中信特钢', '长春高新', '格力电器', '长安汽车', '中油资本', '古井贡酒', '泸州老窖', '云南白药', '徐工机械', '藏格矿业', '潍柴动力', '美的集团', '东方盛虹', '申万宏源', '中联重科', 'TCL科技', '华侨城A', '中兴通讯', 'Ford']
-stock_name = ['Ford', '宏发股份', '药明康德', '恒瑞医药', '复星医药', '晨光股份', '兆易创新', '欧派家居', '福斯特', '今世缘', '正泰电器', '上海医药', '广汽集团', '中国铁建', '恒立液压', '东方电气', '华域汽车', '国电南瑞', '重庆啤酒', '上汽集团', '海康威视', '三花智控', '双汇发展', '振华科技', '长春高新', '海天味业', '宝信软件', '海尔智家', '福耀玻璃', '青岛啤酒', '白云山', '同仁堂', '亿联网络', '汇川技术']
+# stock_name = ['Ford', '宏发股份', '药明康德', '恒瑞医药', '复星医药', '晨光股份', '兆易创新', '欧派家居', '福斯特', '今世缘', '正泰电器', '上海医药', '广汽集团', '中国铁建', '恒立液压', '东方电气', '华域汽车', '国电南瑞', '重庆啤酒', '上汽集团', '海康威视', '三花智控', '双汇发展', '振华科技', '长春高新', '海天味业', '宝信软件', '海尔智家', '福耀玻璃', '青岛啤酒', '白云山', '同仁堂', '亿联网络', '汇川技术']
 # stock_name = ['宏发股份', '药明康德', '恒瑞医药']
 
 ### stock_Top_list is to summarize the performance of each stock
@@ -281,8 +281,12 @@ def report_from_East_Money(url):
 
         ### PE Ratio of the Company ###
         stock_PE_ratio_target = 15  # 这个是目标市盈率，股份不超过这个可以考虑入手
-        stock_price_less_than_PE_ratio_y = stock_PE_ratio_target * \
-            stock_0_profit_margin_y * 4  # 股份不能超过的值
+        if 'INCOMEQC' in url_eastmoney_income: # meaning it is Seasonly data:
+            stock_price_less_than_PE_ratio_y = stock_PE_ratio_target * \
+                stock_0_profit_margin_y * 4  # 股份不能超过的值
+        else: # Meaning it is yealy data, no need to x4
+            stock_price_less_than_PE_ratio_y = stock_PE_ratio_target * \
+                stock_0_profit_margin_y  # 股份不能超过的值
         stock_price_less_than_PE_ratio_y.name = '市盈率15对应股价 元'
 
         stock_output_y = pd.concat([stock_0_TotalRevenue_y, stock_0_TotalAssets_y, stock_0_EBIT_y, stock_0_CurrentAssets_y, stock_0_CurrentLiabilities_y, stock_0_CurrentAssets_vs_Liabilities_y, stock_0_TotalNonCurrentLiabilitiesNetMinorityInterest_y, stock_0_CurrentAssets_minus_TotalNonCurrentLiabilities_y, stock_0_OrdinarySharesNumber_y, stock_0_profit_margin_y, stock_0_profit_margin_increase_y, stock_0_BookValue_per_Share_y, stock_price_less_than_BookValue_ratio_y, stock_price_less_than_PE_ratio_y], axis=1)
@@ -551,7 +555,9 @@ def get_stock_info_for_F():
     stock_price_output = stock_price_output.rename(index={0: '后一年股价范围'})
 
     stock_output_combined = pd.concat([stock_output, stock_price_output], axis=0)
-    return stock_output_combined
+    stock_name_for_F = 'Ford'
+
+    return [stock_output_combined, stock_name_for_F]
 
 
 
@@ -674,9 +680,6 @@ for iii in range(0, len(stock_code)):  # 在所有的沪深300成分股里面进
 
     print('-----Stock No.{}---Begin of {}: ↓ ↓ ↓ ↓ ↓  ---------------\n'.format(iii, stock))
 
-    stock_Top_temp.append('{}--{}-{}'.format(iii, stock, stock_name[iii]))
-
-
     ### to get the yearly report from the East Money ################################
     print('------- To Get the [- yearly -] report from DataBase ------------\n')
     
@@ -709,7 +712,10 @@ for iii in range(0, len(stock_code)):  # 在所有的沪深300成分股里面进
     if data_get_data.status_code == 404: # no data, so we need to save it this time...
         print('---------No data saved before, it\'s time to save it...---------\n')
         if stock == 'F':
-            stock_output_combined = get_stock_info_for_F()
+            stock_output_combined_out = get_stock_info_for_F()
+            stock_output_combined = stock_output_combined_out[0]
+            stock_name = stock_output_combined_out[1]
+
             save_data_to_OneDrive_newFile(stock_output_combined)
             stock_output_yearly = stock_output_combined
         
@@ -738,7 +744,9 @@ for iii in range(0, len(stock_code)):  # 在所有的沪深300成分股里面进
         yearly_report_from_OD = pickle.loads(data_get_data_content.content)
 
         if stock == 'F':
-            stock_output_combined = get_stock_info_for_F()
+            stock_output_combined_out = get_stock_info_for_F()
+            stock_output_combined = stock_output_combined_out[0]
+            stock_name = stock_output_combined_out[1]
 
             ### to update data, keep the info just get, and remove the outdated info from OD
             temp_output = pd.merge(stock_output_combined, yearly_report_from_OD, left_index=True, right_index=True, suffixes=('', '_y'))
@@ -774,7 +782,9 @@ for iii in range(0, len(stock_code)):  # 在所有的沪深300成分股里面进
                     print(':::: It\'s time to update the data now ...   ::::\n')
                     ### get the yearly report date ################################
                     url_yearly = Year_report_url()
-                    yearly_report_raw = report_from_East_Money(url_yearly)[0]
+                    yearly_report_raw_out = report_from_East_Money(url_yearly)
+                    yearly_report_raw = yearly_report_raw_out[0]
+                    stock_name = yearly_report_raw_out[1]
 
                     stock_output_yearly = yearly_report_raw
 
@@ -804,7 +814,9 @@ for iii in range(0, len(stock_code)):  # 在所有的沪深300成分股里面进
         report_notification_date_yearly = stock_output_yearly.loc['Notice Date']
 
         url_seasonly = Seasonly_report_url(report_notification_date_yearly)
-        Seasonly_report_raw = report_from_East_Money(url_seasonly)[0]
+        Seasonly_report_raw_out = report_from_East_Money(url_seasonly)
+        Seasonly_report_raw = Seasonly_report_raw_out[0]
+        stock_name = Seasonly_report_raw_out[1]
 
         report_notification_date_Seasonly = Seasonly_report_raw.loc['Notice Date']
         stock_output_Seasonly = Seasonly_report_raw
@@ -821,7 +833,7 @@ for iii in range(0, len(stock_code)):  # 在所有的沪深300成分股里面进
         stock_output_combined = pd.concat([stock_output_Seasonly_f, stock_output_yearly_f], axis=1)
 
 
-
+    stock_Top_temp.append('{}--{}-{}'.format(iii, stock, stock_name))
 
 
     ### to get the latest 7 days stock price #########################################
@@ -885,27 +897,27 @@ for iii in range(0, len(stock_code)):  # 在所有的沪深300成分股里面进
 
     else:
         page_content = "<div><p>{}--{}-{}, {}</p></div>".format(
-            iii, stock, stock_name[iii], profit_margin_performance)
+            iii, stock, stock_name, profit_margin_performance)
         page_content += "<div><p>{}--{}-{}, {}</p></div>".format(
-            iii, stock, stock_name[iii], CurrentAssets_vs_Liabilities_performance)
+            iii, stock, stock_name, CurrentAssets_vs_Liabilities_performance)
         page_content += "<div><p>{}--{}-{}, {}</p></div>".format(
-            iii, stock, stock_name[iii], dividends_perofrmance)
-        # page_content += "<div><p>This is the output for No. #{} ---{}: {}</p></div>".format(iii, stock, stock_name[iii],)
+            iii, stock, stock_name, dividends_perofrmance)
+        # page_content += "<div><p>This is the output for No. #{} ---{}: {}</p></div>".format(iii, stock, stock_name,)
         page_content += stock_output_combined.to_html()
         page_content += "<div><p>This is the last 7 days stock price for {} {}: {}</p></div>".format(
-            stock, stock_name[iii], last_7_days_stock_price_high_low)
+            stock, stock_name, last_7_days_stock_price_high_low)
         # page_content += "<div><p>This is the dividend for {}: {}</p></div>".format(
-            # stock, stock_name[iii])
+            # stock, stock_name)
         if stock_0_dividends.empty:
             page_content += "<div><p>No dividend record for {}: {}</p></div>".format(
-                stock, stock_name[iii])
+                stock, stock_name)
         else:
             if len(stock_0_dividends) < 15:
                 page_content += stock_0_dividends.to_frame().to_html()
             else:
                 page_content += stock_0_dividends[-13:].to_frame().to_html()
 
-        # page_content += "<div><p>{}--{}-{}, {}</p></div>".format(iii, stock, stock_name[iii],dividends_perofrmance)
+        # page_content += "<div><p>{}--{}-{}, {}</p></div>".format(iii, stock, stock_name,dividends_perofrmance)
         # page_content += "<div><p>--------Complete this one : ↑ ↑ ↑ ↑ ↑  ---------------------</p></div>"
         page_content += "<div><p>                                                                                                </p></div>"
         # page_content = page_content.replace('\n','')
@@ -913,17 +925,17 @@ for iii in range(0, len(stock_code)):  # 在所有的沪深300成分股里面进
 
         # stock_output.to_excel('{}-Output.xlsx'.format(stock),header=1, index=1, encoding='utf_8_sig')
         print('{}--{}-{}'.format(iii, stock,
-              stock_name[iii]), profit_margin_performance, '\n')
+              stock_name), profit_margin_performance, '\n')
         print('{}--{}-{}'.format(iii, stock,
-              stock_name[iii]), CurrentAssets_vs_Liabilities_performance, '\n')
+              stock_name), CurrentAssets_vs_Liabilities_performance, '\n')
         print('{}--{}-{}'.format(iii, stock,
-              stock_name[iii]), dividends_perofrmance, '\n')
+              stock_name), dividends_perofrmance, '\n')
         print('This is the output for No. #{} ---{}: {} \n'.format(iii,
-              stock, stock_name[iii]))
+              stock, stock_name))
         print(tabulate(stock_output_combined, headers='keys', tablefmt='simple'))
         print('This is the last 7 days stock price for {} {}: {} \n'.format(
-            stock, stock_name[iii], last_7_days_stock_price_high_low))
-        # print('This is the dividend for {}: {} \n'.format(stock, stock_name[iii]))
+            stock, stock_name, last_7_days_stock_price_high_low))
+        # print('This is the dividend for {}: {} \n'.format(stock, stock_name))
         print(stock_0_dividends)
         print('--------Complete this one : ↑ ↑ ↑ ↑ ↑  ---------------------\n')
         print('                                                                                                \n')
