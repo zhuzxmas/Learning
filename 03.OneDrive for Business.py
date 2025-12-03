5c8e85e83fd0f4b707ab990ec6156e6c0ff6503b
Nathan Zhu <zzhu25@ford.com>
2025-12-03 23:01:55 +0800

    introduce expiration date check for refresh token

--- 03.OneDrive for Business.py
+++ 03.OneDrive for Business.py
@@ -1,11 +1,34 @@
 import json, requests, datetime, os
 from pandas import DataFrame
-from datetime import datetime
+from datetime import datetime, timezone
 import funcLG
 
-login_return = funcLG.func_login() # to login into MS365 and get the return value info.
-result = login_return['result']
-proxies = login_return['proxies']
+login_return_secret = funcLG.func_login_secret() # to login into MS365 and get the return value info.
+result_secret = login_return_secret['result']
+access_token_secret = result_secret['access_token']
+login_return_refresh = funcLG.get_refresh_token_from_SP(access_token_secret)
+refresh_token = login_return_refresh[0]
+refresh_token_obtained_date_str = login_return_refresh[1]
+proxies = login_return_secret['proxies']
+
+# Parse the datetime string into a timezone-aware datetime object
+refresh_token_obtained_date_date = datetime.fromisoformat(refresh_token_obtained_date_str.replace('Z', '+00:00'))
+
+# Get current time in UTC (timezone-aware)
+now_date = datetime.now(timezone.utc)
+
+# Calculate the difference
+delta_days_date = now_date - refresh_token_obtained_date_date 
+
+# Get the number of days (as a float, but we can use .days for whole days)
+delta_days_number = delta_days_date.days
+
+# Check condition
+if delta_days_number < 40:
+    print("Refresh Token is still ok to use.\n")
+else:
+    print("Refresh Token will expire soon, let's update it now...\n")
+
 
 # to get the Pictures folder id from OneDrive for Business:
 
