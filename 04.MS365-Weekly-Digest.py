ae98723d2157491762ea0c411d525f6fbb35a7e1
Nathan Zhu <92808165+zhuzxmas@users.noreply.github.com>
2026-06-30 08:54:22 +0800

    update bingPic, optimize MS365 weekly digest

--- 04.MS365-Weekly-Digest.py
+++ 04.MS365-Weekly-Digest.py
@@ -38,12 +38,12 @@ today = datetime.now().strftime('%Y-%m-%d')
 if delta_days_number < 80:
     print("\nRefresh Token is still ok to use.\n")
     access_token_with_refresh_token = funcLG.get_access_token_with_refresh(refresh_token=refresh_token)
-    if access_token_with_refresh_token is not '':
+    if access_token_with_refresh_token != '':
         print("Access token got successfully with refresh token!\n")
     else:
         print("Failed to get access token with refresh token, something wrong with current refresh token.\n")
 
-if delta_days_number >= 80 or access_token_with_refresh_token is '':
+if delta_days_number >= 80 or access_token_with_refresh_token == '':
     print("Refresh Token will or has expire(d) soon, let's update it now...\n")
     login_return = funcLG.func_login() # to login into MS365 and get the return value info.
     result_login = login_return['result']
